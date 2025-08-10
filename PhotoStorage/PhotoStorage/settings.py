0f65142dc8e1063a3035b8a9dd9174fe360cda6a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-10 23:36:40 +0330

    iamge db error fixed

--- PhotoStorage/PhotoStorage/settings.py
+++ PhotoStorage/PhotoStorage/settings.py
@@ -111,6 +111,20 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- PhotoStorage/gallery/views.py
+++ PhotoStorage/gallery/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from .models import Product
-
-
+from django.http import HttpResponse
 
 def product_list(request):
-    
+
     products = Product.objects.all()
     context = {
         'products' : products
     }
-    
+
     return render(request , 'gallery/index.html' , context)
+
+
+def home(request):
+    
+    return HttpResponse('this is home')
 

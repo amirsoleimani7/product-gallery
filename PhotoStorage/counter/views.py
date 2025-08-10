ce46a529e36650494e15121c9c98b9a3f429cddc
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-11 00:41:05 +0330

    counting fixed , jsut rendreing left

--- PhotoStorage/counter/views.py
+++ PhotoStorage/counter/views.py
@@ -3,9 +3,25 @@ from django.shortcuts import render
 
 def word_counter(request):
     
+    text = ''
+
     if request.method == 'POST':
         print(f"the text is : {request.POST.get('mytest' , 'didnt get the thing')}")
-    context = {}
+        text = request.POST.get('mytest' , '')
 
+    char_counter = 0
+
+    if text:
+        char_counter = text.count(" ") + 1
+        print(f"there are {char_counter} in the text")
+    else:
+        print(f"there are non in the text")
+    
+
+    context = {
+        'text' : text , 
+        'char_counter' : char_counter , 
+    }
+    
     return render(request , 'counter/index.html' , context)
 

9aa2f628f6edc89218f6dd9da524c549755c1693
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-11 01:38:15 +0330

    tranlation working

--- PhotoStorage/translator/views.py
+++ PhotoStorage/translator/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from translate import Translator
+import pyjokes
 
 
 def traslate(request):
 
+    text = ''
+    to = ''
+    if request.POST:
+        print(f"text to translate is : {request.POST.get('text')}")
+        print(f"to : {request.POST.get('lang')}")
 
-    context= {}
+        text = request.POST.get('text')
+        to = request.POST.get('lang')
+
+
+    translator = Translator(to_lang=to)
+    translation = translator.translate(text)
+
+    print(f"the translation is : {translation}")
+
+    context= {
+        'text' : text,
+        'translation' :  translation ,
+        'joke' : 
+    }
     return render(request , 'translator/index.html' , context)
 

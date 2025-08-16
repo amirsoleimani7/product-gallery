320bc4b0dac5ee6bde1741e6a303726bae94328b
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-16 14:37:41 +0330

    email sending works just fine

--- PhotoStorage/PhotoStorage/settings.py
+++ PhotoStorage/PhotoStorage/settings.py
@@ -130,6 +130,17 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# setting up email stuff here 
+EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'amirsoleimani123456789@gmail.com'
+EMAIL_HOST_PASSWORD = 'ocmtsvfmptapqhqp'
+
+
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- PhotoStorage/email_sender/views.py
+++ PhotoStorage/email_sender/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def home(request):
+    context = {}
 
-    return render(request ,'email_sender/index.html' , context = {})
+    if request.POST:
+        end_email = request.POST.get('address')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        print(f"end email is : {end_email}")
+        print(f"subject is : {subject}")
+        print(f"message is : {message}")
+
+
+        
+        if end_email and subject and message :
+            try :
+                send_mail(subject ,message , settings.EMAIL_HOST_USER ,[end_email]) 
+                context['res'] = 'Email sent succesfully'
+            except Exception as e:
+                context['res'] = f'error sending email :{e}'
+        else :  
+            context['res'] = 'you should fill all the feilds'
+
+    else :
+        print(f'the request wast even post')
+
+
+    return render(request ,'email_sender/index.html' , context)
